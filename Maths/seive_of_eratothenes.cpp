38fbc9d998aaa96fce775ae3e24edf7dcf1374e9
Abhay Kumar Ray <abhayray2002@gmail.com>
2021-09-27 23:16:33 +0530

    Seive of Eratothenes added

---
+++ Maths/seive_of_eratothenes.cpp
@@ -0,0 +1,27 @@
+// Used to print all primes smaller than or equal to n.
+
+#include <iostream>
+
+using namespace std;
+
+void seive(int n){
+    bool primes[n+1];
+    for(int i = 2; i*i <= n;i++){
+        if(!primes[i]){
+            for(int j = i*2 ; j <= n; j+=i){
+                primes[j] = true;
+            }
+        }
+    }
+    for(int i = 2; i <= n; i++){
+        if(!primes[i]){
+            cout<<i<<endl;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    seive(n);
+}
