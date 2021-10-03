6535f2a1e8983db1d6025dfc3c210d2323e95d41
Abhay Kumar Ray <abhayray2002@gmail.com>
2021-10-03 23:28:57 +0530

    Trailing Zeros

---
+++ Maths/9_trailing_zeros.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int trialingZeros(int n){
+    int cnt = 0;
+    for(int i = 5; i < n;i*=5){
+        cnt += n/i;
+    }
+    return cnt;
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<trialingZeros(n)<<endl;
+}
